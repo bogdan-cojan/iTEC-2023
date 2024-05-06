200815ddc95b596315a3f20599025c7c9aee4fb4
Bogdan I. Cojan <60678383+bogdan-cojan@users.noreply.github.com>
2024-05-06 23:11:14 +0300

    Testing vscode in browser

--- app/controllers/apis/v1/poems_controller.rb
+++ app/controllers/apis/v1/poems_controller.rb
@@ -1,6 +1,6 @@
 class Apis::V1::PoemsController < ApplicationController
   include Apis::V1::PoemData
-  
+  #test vscode.dev
   def show
     poem = Apis::V1::PoemData::POEMS.sample
     render json: { title: poem[:title], author: poem[:author], text: poem[:text] }
